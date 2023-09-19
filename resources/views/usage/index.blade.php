b71fa68451fa5080851447ba9372d2733b36936d
rakaarif <raka.arif@sohoglobalhealth.com>
2023-09-19 11:59:07 +0700

    edit view list material usage

--- resources/views/usage/index.blade.php
+++ resources/views/usage/index.blade.php
@@ -67,64 +67,14 @@
                                 <td>{{ $usages->no_batch }}</td>
                                 <td>{{ $usages->Expire_Date }}</td>
                                 <td>{{ $usages->Status == 1 ? 'Aktif' : 'InAktif' }}</td>
+                            </tr>
                         @endforeach
-
-                        </tr>
-
                     </tbody>
                 </table>
             </div>
 
         </div>
     </div>
-            <th class="w-1 ml-1" style="background-color: lightgray;">Action</th>
-        <th style="background-color: lightgray;">No</th>
-       
-        <th style="background-color: lightgray;">Catalog Number</th>
-        <th style="background-color: lightgray;">Name of Material</th>
-        <th style="background-color: lightgray;">Type of Material</th>
-        <th style="background-color: lightgray;">Quantity</th>
-        <th style="background-color: lightgray;">Unit</th>
-        <th style="background-color: lightgray;">No Batch</th>
-        <th style="background-color: lightgray;">Expire Date</th>
-        <th style="background-color: lightgray;">Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          @foreach ($usage as $usages)
-          <tr>
-            <td>
-              @if (auth()->user()->leveluser >4)
-                  <form action="{{ route('usage.destroy',$usages->id) }}" method="POST">
-                      <a class="btn btn-primary btn-sm" href="{{ route('usage.edit',$usages->id) }}"title="Edit Barang" > <i class="fa fa-pen"></i></a>
-                  @csrf
-                  @method('DELETE')
-    
-                  <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure want to delete this ?');"title="Delete Barang"><i class="fa fa-trash"></i></button>
-              </form>
-              @endif
-          </td>
-  
-          <td>{{ ++$i }}</td>
-          <td>{{ $usages->Catalog_Number }}</td>
-          <td>{{ $usages->Barang->Name_of_Material }}</td>
-          <td>{{ $usages ->Type_of_Material }}</td>
-          <td>{{ $usages->Quantity }}</td>
-          <td>{{ $usages->Unit }}</td>
-          <td>{{ $usages->no_batch }}</td>
-          <td>{{ $usages->Expire_Date }}</td>
-          <td>{{ $usages->Status == 1 ? 'Aktif' : 'InAktif' }}</td>
-            @endforeach
-            
-          </tr>
-          
-        </tbody>
-      </table>
-    </div>
-    
-  </div>
-</div>
-
     <br>
     <div
         class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-1 border-bottom border-danger">
@@ -138,7 +88,7 @@
 @push('js')
     <script type="text/javascript">
         $(function() {
-            var table = $('#listlow').DataTable({
+            var table = $('#listlow,#listlowss').DataTable({
                 dom: 'lBftrip',
                 buttons: [{
                         extend: 'copyHtml5',
