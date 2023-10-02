e7a331e70010d5b593fa5f2ae6f24e518366a6bc
Kilerbean <kilerbean11@gmail.com>
2023-10-02 11:15:06 +0700

    oke

--- app/Http/Requests/StorebarangRequest.php
+++ app/Http/Requests/StorebarangRequest.php
@@ -21,8 +21,35 @@ class StorebarangRequest extends FormRequest
      */
     public function rules(): array
     {
+
         return [
-            //
+            'Type_of_Material' => ['required'],
+            'Type_of_Budget' => ['required'],
+            'Name_of_Material' => ['required'],
+            'Catalog_Number' => ['required'],
+            'packingsize' => ['required'],
+            'packingsize_unit' => ['required'],
+            'Manufaktur' => ['required'],
+            'Unit' => ['required'],
+            'Safety_Stock' => ['required'],
+            'Harga' => ['required'],
+            'Maximum_Stock' => ['required'],
+            'category' => ['required'],
+        ];
+    }
+
+    /**
+     * Custom validation messages.
+     *
+     * @return array
+     */
+    public function messages(): array
+    {
+        return [
+            'Harga.required' => 'The Prices field is required.',
+            'packingsize.required' => 'The Packing Size field is required.',
+            'packingsize_unit.required' => 'The Packing Size Unit field is required.',
+            'category.required' => 'The Movement Category field is required.',
         ];
     }
 }
