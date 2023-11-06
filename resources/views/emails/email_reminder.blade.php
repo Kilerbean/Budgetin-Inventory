336675737cba6086920741bd773701ac81376f0e
Kilerbean <kilerbean11@gmail.com>
2023-11-06 10:10:53 +0700

    ok

--- resources/views/emails/email_reminder.blade.php
+++ resources/views/emails/email_reminder.blade.php
@@ -1,26 +1,77 @@
 
 <!-- ####### HEY, I AM THE SOURCE EDITOR! #########-->
-<h1><span style="color: #0000ff;">Q-LIS</span></h1>
-<p>&nbsp;</p>
-<h2 style="color: #2e6c80;">Reminder of calibration schedule approaching 1 month of calibration time</h2>
-<table class="editorDemoTable"   style="height: 36px; border-style: solid; border-color: black;" border="1">
-    <thead>
-        <tr>
-            <td>ID Instrument</td>
-            <td>Name Instrument</td>
-            <td>Next Calibration Date</td>
-            <td>Calibrations By</td>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($kalibrasinear as $item)
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Calibration Reminder</title>
+    <style>
+        .header {
+            color: #0000ff;
+            font-size: 24px;
+            margin-bottom: 10px;
+        }
+        .greeting {
+            color: #0000ff;
+            font-size: 18px;
+        }
+        .due-date {
+            color: #2e6c80;
+            font-size: 20px;
+            margin-top: 10px;
+        }
+        .table {
+            border-collapse: collapse;
+            width: 100%;
+            border: 1px solid black;
+            margin-top: 20px;
+        }
+        .table th, .table td {
+            border: 1px solid black;
+            padding: 8px;
+            text-align: left;
+        }
+        .button {
+            background-color: #2e6c80;
+            color: white;
+            padding: 10px 20px;
+            text-decoration: none;
+            border-radius: 5px;
+            display: inline-block;
+            margin-top: 20px;
+        }
+        .footer {
+            font-size: 16px;
+            margin-top: 20px;
+        }
+    </style>
+</head>
+<body>
+    <div class="header">Q-LIS Calibration Reminder</div>
+    <div class="greeting">Dear Bapak/Ibu,</div>
+    <p>&nbsp;</p>
+    <div class="due-date">Due Date Calibration: Approaching 1 Month - Requires Follow-Up</div>
+    <table class="table">
+        <thead>
             <tr>
-                <td>{{ $item->instrumentid }}</td>
-                <td>{{ $item->instrumentname }}</td>
-                <td>{{ $item->nextcalibration }}</td>
-                <td>{{ $item->calibrationby }}</td>
+                <th>ID Instrument</th>
+                <th>Name Instrument</th>
+                <th>Next Calibration Date</th>
+                <th>Calibrations By</th>
             </tr>
-        @endforeach
-    </tbody>
-</table>
-
+        </thead>
+        <tbody>
+            @foreach ($kalibrasinear as $item)
+                <tr>
+                    <td>{{ $item->instrumentid }}</td>
+                    <td>{{ $item->instrumentname }}</td>
+                    <td>{{ $item->nextcalibration }}</td>
+                    <td>{{ $item->calibrationby }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+    <a href="http://172.19.19.39:98" class="button">Login to Calibration System</a>
+    <div class="footer">This email is auto-generated from the Q-LIS Calibration System Database. Please do not reply.</div>
+    <div class="footer">Best Regards</div>
+</body>
+</html>
--- resources/views/emails/email_reminderoneweek.blade.php
+++ resources/views/emails/email_reminderoneweek.blade.php
@@ -1,26 +1,45 @@
 
-<!-- ####### HEY, I AM THE SOURCE EDITOR! #########-->
-<h1><span style="color: #0000ff;">Q-LIS</span></h1>
-<p>&nbsp;</p>
-<h2 style="color: #2e6c80;">Reminder of calibration schedule approaching 1 week of calibration time</h2>
-<table class="editorDemoTable"   style="height: 36px; border-style: solid; border-color: black;" border="1">
-    <thead>
-        <tr>
-            <td>ID Instrument</td>
-            <td>Name Instrument</td>
-            <td>Next Calibration Date</td>
-            <td>Calibrations By</td>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($kalibrasinearweek as $item)
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Calibration Reminder</title>
+    <style>
+        .button {
+            background-color: #2e6c80;
+            color: white;
+            padding: 10px 20px;
+            text-decoration: none;
+            border-radius: 5px;
+            display: inline-block;
+        }
+    </style>
+</head>
+<body>
+    <h2 style="color: #0000ff;">Q-LIS Calibration Reminder</h2>
+    <p>Dear Bapak/Ibu,</p>
+    <p style="color: #2e6c80;">Due Date Calibration: Approaching 1 Week - Requires Follow-Up</p>
+    <table border="1" style="border-collapse: collapse; width: 100%;">
+        <thead>
             <tr>
-                <td>{{ $item->instrumentid }}</td>
-                <td>{{ $item->instrumentname }}</td>
-                <td>{{ $item->nextcalibration }}</td>
-                <td>{{ $item->calibrationby }}</td>
+                <th>ID Instrument</th>
+                <th>Name Instrument</th>
+                <th>Next Calibration Date</th>
+                <th>Calibrations By</th>
             </tr>
-        @endforeach
-    </tbody>
-</table>
-
+        </thead>
+        <tbody>
+            @foreach ($kalibrasinearweek as $item)
+                <tr>
+                    <td>{{ $item->instrumentid }}</td>
+                    <td>{{ $item->instrumentname }}</td>
+                    <td>{{ $item->nextcalibration }}</td>
+                    <td>{{ $item->calibrationby }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+    <a href="http://172.19.19.39:98" class="button">Login to Calibration</a>
+    <p>This email is auto-generated from the Q-LIS Calibration System Database. Please do not reply.</p>
+    <p>Best Regards,</p>
+</body>
+</html>
